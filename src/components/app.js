ac4ae8d2bcc8ef38498579b0e57bc9a717098d50
Kris Dunning <dunning.kris@gmail.com>
2022-11-08 09:56:16 -0800

    attempt to fix app to App problem

---
+++ src/components/app.js
@@ -0,0 +1,17 @@
+import Header from './header/header';
+import Footer from './footer/footer';
+import Storefront from './storefront/storefront';
+import Products from './storefront/products';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Storefront/>
+    <Products/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
